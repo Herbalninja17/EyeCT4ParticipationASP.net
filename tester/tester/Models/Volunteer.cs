df83cee89929a22c14d93ddd5c259add347f4eed
Redxice <melvinnboeters@outlook.com>
2016-06-14 11:08:36 +0200

    volunteer class

--- tester/tester/Models/Volunteer.cs
+++ tester/tester/Models/Volunteer.cs
@@ -7,6 +7,14 @@ namespace tester.Models
 {
     public class Volunteer:User
     {
-
+        public int volunteerID { get; set; }
+        public bool car { get; set; }
+        public bool license { get; set; }
+        public List<Appointment> appointmentList { get; set; }
+        public Volunteer(string name,string email, string address, string city, int phonenumber, int ID) :base(name,email,city, address, phonenumber)
+        {
+            this.volunteerID = ID;
+        }
+        
     }
 }
