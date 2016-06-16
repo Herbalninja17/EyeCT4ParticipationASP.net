beb9e927fdcf6f062f6dc61edd692e80ebc6a155
Raphael Kolijn <raphaelkolijn@gmail.com>
2016-06-16 14:04:12 +0200

    eqrt

--- tester/tester/Controllers/ChatController.cs
+++ tester/tester/Controllers/ChatController.cs
@@ -9,15 +9,15 @@ namespace tester.Controllers
 {
     public class ChatController : Controller
     {
-        // GET: Chat
-        [HttpPost]
-        public ActionResult Chatbox(string msg, int i)
-        {
-            tester.Models.Database.chatsend(3, 2, msg, i);
-            return View();
-        }
+        //// GET: Chat
+        //[HttpPost]
+        //public ActionResult Chatbox(string msg, int i)
+        //{
+        //    tester.Models.Database.chatsend(3, 2, msg, i);
+        //    return View();
+        //}
 
-        [HttpGet]
+        //[HttpGet]
         public ActionResult Chatbox()
         {
             tester.Models.Database.chatbox(3, 2);
