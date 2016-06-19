75d69240e452b51a30c9b463babe6a2466a76a78
Olaf van Dijk <336692@student.fontys.nl>
2016-06-19 16:16:14 +0200

    Eazy E

---
+++ tester/UnitTestEyeCT4Participation/T_BRemove.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tester.Models;
+
+namespace UnitTestEyeCT4Participation
+{
+    [TestClass]
+    public class T_BRemove
+    {
+        [TestMethod]
+        public void BRemoveChat_Test()
+        {
+            //Beheerder maakt een chat invisible voor normale gebruikers
+            Assert.AreEqual(true, Database.alterYorN("CHAT", 1, "CHATID", "ISVISIBLE", "N"));
+
+            //Chat wordt weer visible gemaakt
+            Assert.AreEqual(true, Database.alterYorN("CHAT", 1, "CHATID", "ISVISIBLE", "Y"));
+        }
+
+        [TestMethod]
+        public void BRemoveReview_Test()
+        {
+            //Beheerder maakt een review invisible voor normale gebruikers
+            Assert.AreEqual(true, Database.alterYorN("REVIEW", Database.ItemIDSelected, "REVIEWID", "ISVISIBLE", "N"));
+
+            //Review wordt weer visible gemaakt
+            Assert.AreEqual(true, Database.alterYorN("REVIEW", Database.ItemIDSelected, "REVIEWID", "ISVISIBLE", "Y"));
+        }
+
+        [TestMethod]
+        public void BRemoveHulpvraag_Test()
+        {
+            //Beheerder maakt een hulpvraag invisible voor normale gebruikers
+            Assert.AreEqual(true, Database.alterYorN("HULPVRAAG", Database.ItemIDSelected, "HULPVRAAGID", "ISVISIBLE", "N"));
+
+            //Hulpvraag wordt weer visible gemaakt
+            Assert.AreEqual(true, Database.alterYorN("HULPVRAAG", Database.ItemIDSelected, "HULPVRAAGID", "ISVISIBLE", "Y"));
+        }
+    }
+}
---
+++ tester/UnitTestEyeCT4Participation/T_Inschrijf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tester.Models;
+
+namespace UnitTestEyeCT4Participation
+{
+    [TestClass]
+    public class T_Inschrijf
+    {
+        [TestMethod]
+        public void Inschrijf_Test()
+        {
+            //Registreren en inloggen
+            Database.RegesterUser("Unittest", "Unittest123", "Needy", "unittest@geslaagd.com", "Unittest", "links", "rechts", 0212151, "M", string.Empty, string.Empty, string.Empty, string.Empty);
+            Assert.AreEqual(true, Database.Login("Unittest", "Unittest123"));
+        }
+    }
+}
